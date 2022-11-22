c1b3cefbf3731dbf94a9d3f524a5055646203de6
jdrew153 <jdrew153@gmail.com>
2022-11-22 14:56:37 -0500

    fleshed out stripe payment services

--- handlers/donationHandler.go
+++ handlers/donationHandler.go
@@ -2,8 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/stripe/stripe-go/v72"
 	"github.com/stripe/stripe-go/v72/paymentintent"
@@ -13,33 +11,51 @@ import (
 
 type createPaymentIntentReq struct {
 	PaymentType string `json:"paymentType"`
-	Currency string `json:"currency"`
-	Amount int64 `json:"amount"`
-
+	Currency    string `json:"currency"`
+	Amount      string `json:"amount"`
 }
 
-
+const (
+	unDollares    string = "unDollares"
+	cincoDollares string = "cincoDollares"
+	diazDollares  string = "diazDollares"
+	manyDollares  string = "manyDollares"
+)
 
 func CreateCheckoutSession(c *fiber.Ctx) error {
 	req := createPaymentIntentReq{}
 
-	// unmarshalling the request into the req 
+	// unmarshalling the request into the req
 	err := c.BodyParser(&req)
 
 	if err != nil {
-		return c.SendStatus(500)
+		return c.JSON(err)
 	}
 
+	amount := 0
+
 	fmt.Println(req)
-	if req.Amount <= 1998 {
-		return c.SendString("amount cannot be less than zero you punk bitch")
+
+	switch req.Amount {
+	case unDollares:
+		amount = 1000
+		break
+	case cincoDollares:
+		amount = 5000
+		break
+	case diazDollares:
+		amount = 10000
+		break
+	case manyDollares:
+		amount = 20000
+		break
 	}
 
 	// This is your test secret API key.
-	stripe.Key = os.Getenv("STRIPE_API_KEY")
+	stripe.Key = "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"
 
 	params := &stripe.PaymentIntentParams{
-		Amount: stripe.Int64(req.Amount),
+		Amount:   stripe.Int64(int64(amount)),
 		Currency: stripe.String(req.Currency),
 		PaymentMethodTypes: stripe.StringSlice([]string{
 			req.PaymentType,
